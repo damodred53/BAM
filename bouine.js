6c85b1389dcf8b06563c7c372383fd89c2bd4220
Florent <florent.guyard@live.fr>
2024-06-25 12:32:15 +0200

    petit commit

--- bouine.js
+++ bouine.js
@@ -1,10 +1,10 @@
 /*${isVisible ? 'visible' : 'hidden'}*/
 
 
-<div className="vitrine_second_column">
+/*<div className="vitrine_second_column">
                 {dataBam.map((cardBam) => (
                     cardBam.id >= 7 ? <CardSecondColumn key={cardBam.id} id={cardBam.id} name={cardBam.name} presentation={cardBam.presentation} /> : null
                 ))}
-            </div>
+            </div>*/
 
 /**/
