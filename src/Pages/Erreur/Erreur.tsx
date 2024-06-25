3aa087c7d5faed77a9e6433aca48c6fe7b364e2a
Florent <florent.guyard@live.fr>
2024-06-25 11:54:35 +0200

    modification page erreur

--- src/Pages/Erreur/Erreur.tsx
+++ src/Pages/Erreur/Erreur.tsx
@@ -1,10 +1,14 @@
+import Footer from "../../Composants/Footer/Footer";
+import NavBar from "../../Composants/NavBar/NavBar";
 
 const Erreur = () => {
 
     return (
-
-        <h1>Ceci est la page d'erreur de la BAM</h1>
-
+        <div>
+            <NavBar />
+            <h1>Ceci est la page d'erreur de la BAM</h1>
+            <Footer />
+        </div>
     )
 }
 
