f5f496321c3feba8e9388785ef8441179b88a3f7
Florent <florent.guyard@live.fr>
2024-06-27 10:03:35 +0200

    ok 4,5

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/description' element={<Description />}></Route>
         <Route path='/legales' element ={ <MentionLegales />}></Route>
         <Route path='/vente' element={<Vente />}></Route>
-        <Route path="/BAM/" element={<LandingPages />}></Route>
+        <Route path="/BAM" element={<LandingPages />}></Route>
       </Routes>
     </Router>
   </React.StrictMode>,
